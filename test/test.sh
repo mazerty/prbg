865e3ec30e6f3723b0f36f13c1990244cbd554b7
mazerty <mazerty@gmail.com>
2018-03-04 13:46:29 +0100

    writes the bytearray to stdout instead of a file

--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,13 @@
 #!/bin/sh -ex
 
+# cleans a potentially failed previous test run
+[ -d tmp/ ] && rm -rf tmp/
+
 # creates tmp directory
 mkdir tmp
 
 # test
-../src/prfg test 128 tmp/test
+../src/prfg test 128 > tmp/test
 diff tmp/test results/test
 
 # cleans tmp directory
